ba9fed6cd4a60d183c3c85970487b5905b8b270d
Eugênio Barros <eugenio.barros.jr@gmail.com>
2026-07-20 09:32:33 -0300

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0
+        let r = nums.length - 1
+
+        while(l <= r) {
+            let m = l + Math.floor((r - l) / 2)
+
+            if(target === nums[m]) {
+                return m
+            }
+            if(nums[m] > target) {
+                r = m - 1
+            }
+            else {
+                l = m + 1
+            }
+        }
+
+
+        return -1
+    }
+}
